cfa0446bb0e63c1c7a0c2da9d427b06c5125d8b3
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2017-07-16 01:49:05 -0400

    cambios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function token(){
-        return json_encode(csrf_field());
+        return csrf_field();
     }
     public function login(Request $request){
         return $request->all();
